7d8c8c3809f9587fc97038743a8dbb895baee8ae
Renato José de Meira <renato.meira@itixti.com.br>
2023-04-20 10:31:21 -0300

    Projeto Inicial

---
+++ Usuario.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModeloBase
+{
+    public class Usuario
+    {
+        public int IdUsuario { get; set; }
+        public string Login { get; set; } = string.Empty;
+        public string Senha { get; set; }
+    }
+}
